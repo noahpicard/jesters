d372bc040fd6926ed7dd3b66e847bd5a9712e13a
Noah Picard <noahapicard@gmail.com>
2023-06-15 12:06:07 +0100

    fix: Include tornado web import

--- server/main.py
+++ server/main.py
@@ -7,6 +7,7 @@ import string
 import time
 
 from tornado.options import define, options, parse_command_line
+import tornado.web
 
 define("port", default=8888, help="run on the given port", type=int)
 define("debug", default=True, help="run in debug mode")
